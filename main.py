b63f7d1ada9ceca537679b1ce635c638504dc298
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-10-12 14:35:40 +0530

    Add : Send SMS functionality to notify about the percentage change and news related to the input stock

--- main.py
+++ main.py
@@ -3,21 +3,16 @@ import os
 import requests
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from twilio.rest import Client
+
 
 load_dotenv()
 
-STOCK = "TATAMOTORS"
-COMPANY_NAME = "Tata Motors"
+STOCK = "RELIANCE"
+COMPANY_NAME = "Reliance Industries"
 EXCHANGE_NAME = ".BSE"
-
-# STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 STOCK_DETAILS_API_ENDPOINT = "https://www.alphavantage.co/query"
-stock_details_params = {
-    "function": "TIME_SERIES_DAILY",
-    "symbol": STOCK + EXCHANGE_NAME,
-    "apikey": os.environ.get("ALPHA_VANTAGE_API_KEY")
-}
+NEWS_API_ENDPOINT = "https://newsapi.org/v2/everything"
 
 # Get yesterday's and day before yesterday's date in the format yyyy-mm-dd
 today = datetime.now()
@@ -28,47 +23,96 @@ today = today.strftime("%Y-%m-%d")
 yesterday = yesterday.strftime("%Y-%m-%d")
 day_before_yesterday = day_before_yesterday.strftime("%Y-%m-%d")
 
-# Get stock price details for yesterday and day before yesterday
-stock_details_response = requests.get(url=STOCK_DETAILS_API_ENDPOINT, params=stock_details_params)
-stock_details_response.raise_for_status()
-stock_data = stock_details_response.json()
+# STEP 1: Use https://www.alphavantage.co
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+def get_stock_percentage_change(stock_name: str, exchange_name: str) -> float:
+    stock_details_params = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": stock_name + exchange_name,
+        "apikey": os.environ.get("ALPHA_VANTAGE_API_KEY")
+    }
 
-stock_price_yesterday = stock_data["Time Series (Daily)"][yesterday]["4. close"]
-stock_price_yesterday = float(stock_price_yesterday)
+    # Get stock price details for yesterday and day before yesterday
+    stock_details_response = requests.get(url=STOCK_DETAILS_API_ENDPOINT, params=stock_details_params)
+    stock_details_response.raise_for_status()
+    stock_data = stock_details_response.json()
 
-stock_price_day_before_yesterday = stock_data["Time Series (Daily)"][day_before_yesterday]["4. close"]
-stock_price_day_before_yesterday = float(stock_price_day_before_yesterday)
+    stock_price_yesterday = stock_data["Time Series (Daily)"][yesterday]["4. close"]
+    stock_price_yesterday = float(stock_price_yesterday)
 
-percentage_change = (stock_price_yesterday - stock_price_day_before_yesterday) / stock_price_day_before_yesterday * 100
-print(f"Percentage change for {COMPANY_NAME} is {round(percentage_change, 2)}%")
+    stock_price_day_before_yesterday = stock_data["Time Series (Daily)"][day_before_yesterday]["4. close"]
+    stock_price_day_before_yesterday = float(stock_price_day_before_yesterday)
+
+    percentage_change = (stock_price_yesterday - stock_price_day_before_yesterday) / stock_price_day_before_yesterday * 100
+    print(f"Percentage change for {stock_name} is {round(percentage_change, 2)}%")
+    return round(percentage_change, 2)
+
+
 
 # STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-NEWS_API_ENDPOINT = "https://newsapi.org/v2/everything"
-news_params = {
-    "q": COMPANY_NAME,
-    "from": yesterday,
-    "sortBy": "popularity",
-    "apiKey": os.environ.get("NEWS_API_KEY")
-}
 
-news_response = requests.get(url=NEWS_API_ENDPOINT, params=news_params)
-news_response.raise_for_status()
-news_data = news_response.json()
-top_3_articles = news_data["articles"][:3]
-
-for article in top_3_articles:
-    print(article)
-    formatted_article = f"""
-    Source: {article["source"]["name"]}
-    Title: {article["title"]}
-    Full Article: {article["url"]}
-    Published at: {article["publishedAt"]}
+def get_stock_news(company_name, news_date) -> list:
     """
+    Get top 3 news articles on the stock/company
+    """
+
+    news_params = {
+        "q": company_name,
+        "from": news_date,
+        "sortBy": "popularity",
+        "apiKey": os.environ.get("NEWS_API_KEY")
+    }
+
+    news_response = requests.get(url=NEWS_API_ENDPOINT, params=news_params)
+    news_response.raise_for_status()
+    news_data = news_response.json()
+    top_3_articles = news_data["articles"][:3]
+
+    formatted_articles = []
+    for index in range(len(top_3_articles)):
+        article = top_3_articles[index]
+        formatted_article = f"""
+        {index + 1}. {article["title"]}
+        Source: {article["source"]["name"]}
+        Full Article: {article["url"]}
+        Published at: {article["publishedAt"]}
+        """
+        formatted_articles.append(formatted_article)
+
+    return formatted_articles
+
 
 # STEP 3: Use https://www.twilio.com
 # Send a separate message with the percentage change and each article's title and description to your phone number.
+def send_stock_update_sms() -> None:
+    """
+    This function sends an SMS to the user notifying the drastic change in the stock price and related news
+    """
+    account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+    auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+    client = Client(account_sid, auth_token)
 
+    stock_percent_change = get_stock_percentage_change(stock_name=STOCK, exchange_name=EXCHANGE_NAME)
+    top_3_articles = get_stock_news(company_name=COMPANY_NAME, news_date=yesterday)
+
+    direction_emoji = "🔺" if  stock_percent_change >= 0 else "🔻"
+    body = f"{STOCK}: {direction_emoji}{abs(stock_percent_change)}%"
+
+    for index in range(len(top_3_articles)):
+        body += f"\n{top_3_articles[index]}"
+
+    message = client.messages.create(
+        body=body,
+        from_=os.environ.get("TWILIO_PHONE_NUMBER"),
+        to=os.environ.get("MY_PHONE_NUMBER"),
+    )
+
+    print(message.sid)
+
+
+if __name__ == "__main__":
+    send_stock_update_sms()
 
 # Optional: Format the SMS message like this:
 """
